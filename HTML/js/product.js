69b104a55b234a38834f0190cd75b2937a3ccbb7
Binh Quoc <hongquocbinh1812@gmail.com>
2021-02-21 16:03:54 +0700

    complet giao dien blog.html(chi tiet san pham)

--- HTML/js/product.js
+++ HTML/js/product.js
@@ -1,3 +1,16 @@
+//so luong san pham
+let varObj = [1];
+let soLuong = 1;
+let weight = 0;
+let productPost = {
+  name: "binh",
+  address: "",
+  email: "",
+  phone: "",
+  count: "",
+  weight: "",
+  price: "",
+};
 const fetchProductDetail = function () {
   //const courseld = window.location.search.split("=")[1]; //co the bo window ,spilt la ham tach chuoi thanh mang
   const courseId = window.location.search.split("=")[1]; //lấy chuổi sau dấu =  trên thanh search
@@ -15,7 +28,7 @@ const fetchProductDetail = function () {
     })
     .catch(function (err) {
       //console.log(err);
-      console.log("nono");
+      console.log("nono1");
     });
 };
 
@@ -35,139 +48,165 @@ const fetchAllProduct = function () {
     })
     .catch(function (err) {
       //console.log(err);
-      console.log("nono");
+      console.log("nono2");
     });
 };
+function numberFormat(nStr) {
+  nStr += "";
+  x = nStr.split(".");
+  x1 = x[0];
+  x2 = x.length > 1 ? "." + x[1] : "";
+  var rgx = /(\d+)(\d{3})/;
+  while (rgx.test(x1)) x1 = x1.replace(rgx, "$1" + "," + "$2");
+  return x1 + x2;
+}
 
 fetchProductDetail();
 function renderProduct(data) {
+  let varPrice = [data.price];
+  let priceFormat = numberFormat(data.price);
   let productContent = "";
   let modalContent = "";
+  weightAll = [200];
   productContent = `
   <div class="entry-thumbnail col-sm-6">
   <img alt="" src="${data.image}" />
 </div>
 <div class="priceProduce col-sm-6">
   <h3>${data.title}</h3>
-  <h4>Giá từ : <span>${data.price}</span> VND</h4>
+  <h4>Giá từ : <span>${priceFormat}</span> VND</h4>
   <p>
   ${data.content}
   </p>
   <h6>Trọng Lượng</h6>
-  <button class="draw" onclick="getWeight(100)">100gr</button>
-  <button class="draw" onclick="getWeight(200)">200gr</button>
-  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  <button class="draw" onclick="getWeight(100,${data.price})">100gr</button>
+  <button class="draw" onclick="getWeight(200,${data.price})">200gr</button>
+  <button type="button" class="btn btn-primary"  data-toggle="modal" data-target="#exampleModal">
   Mua Ngay
 </button>
 </div>
   `;
   // modal content
   modalContent = `
-  <div class="modal-header text-center">
-  <h5 class="modal-title" id="exampleModalLabel">
-    ${data.title}
-  </h5>
-  <button
-    type="button"
-    class="close"
-    data-dismiss="modal"
-    aria-label="Close"
-  >
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
-<div class="modal-body">
-  <div class=".Contact__content col-12">
-    <h1>Đăng kí mua</h1>
-    <div class="contact__list mt-5">
-      <form action="" name="form1">
-        <input
-          id="input__name"
-          class="pl-3"
-          type="text"
-          name="HoTen"
-          placeholder="Name"
-        />
-        <span
-          class="sp-thongbao"
-          id="firstNameError"
-        ></span>
-        <textarea
-          id="input__area"
-          class="pl-3 pt-3"
-          name="NoiDung"
-          id=""
-          cols="20"
-          rows="10"
-          placeholder="Message"
-        ></textarea>
-        <span class="sp-thongbao" id="areaError"></span>
-        <input
-          id="input__email"
-          class="pl-3"
-          type="email"
-          name="email"
-          placeholder="email"
-        />
-        <span
-          class="sp-thongbao"
-          id="emailError"
-        ></span>
-        <input
-          id="input__phone"
-          class="pl-3"
-          type="number"
-          name="phone"
-          placeholder="phone"
-        />
-        <span
-          class="sp-thongbao"
-          id="phoneError"
-        ></span>
-        <input
-          id="input__count"
-          class="pl-3"
-          type="number"
-          name="count"
-          placeholder="số lượng"
-          max="2"
-        />
-        <span
-        class="sp-thongbao"
-        id="countError"
-      ></span>
-
-      <input
-          id="input__weight"
-          class="pl-3"
-          type="number"
-          name="weight"
-          placeholder="trọng lượng"
-        />
-     
-        <p style="margin: 0">Giá trị ${data.price}</p>
-        <button
-          id="button__submit"
-          type="button"
-          class="btn"
-          onclick="submitClick()"
+    <div class="modal-header text-center">
+    <h5 class="modal-title" id="exampleModalLabel">
+      ${data.title}
+    </h5>
+    <button
+      type="button"
+      class="close"
+      data-dismiss="modal"
+      aria-label="Close"
+      onclick="closeModal()"
+    >
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="modal-body">
+    <div class=".Contact__content col-12">
+      <h1>Đăng kí mua</h1>
+      <div class="contact__list mt-5">
+        <form name="form1">
+          <input
+            id="input__name"
+            class="pl-3"
+            type="text"
+            name="HoTen"
+            placeholder="Họ và Tên"
+          />
+          <span
+            class="sp-thongbao"
+            id="firstNameError"
+          ></span>
+          <textarea
+            id="input__area"
+            class="pl-3 pt-3"
+            name="NoiDung"
+            id=""
+            cols="20"
+            rows="10"
+            placeholder="Địa chỉ"
+          ></textarea>
+          <span class="sp-thongbao" id="areaError"></span>
           
-        >
-          submit
-        </button>
-      </form>
+          <input
+            id="input__email"
+            class="pl-3"
+            type="email"
+            name="email"
+            placeholder="email"
+          />
+          <span
+            class="sp-thongbao"
+            id="emailError"
+          ></span>
+          <input
+            id="input__phone"
+            class="pl-3"
+            type="number"
+            name="phone"
+            placeholder="Sồ điện thoại"
+          />
+          <span
+            class="sp-thongbao"
+            id="phoneError"
+          ></span>
+          <input id="input__reset" type="reset" value = "Nhập lại" />
+          <button type="button" class="btnOfCount" id="btn__plus" onclick="changeCountPlus(${varPrice[0]})">+</button>
+          <div class="parentIp__count">
+          <p style="margin=0;">Số Lượng</p>
+          <p id="ip__count">${soLuong}</p>
+          </div>
+          
+          <button type="button" class="btnOfCount" id="btn__minus" onclick="changeCountMinus(${varPrice[0]})">-</button>
+          <span
+          class="sp-thongbao"
+          id="countError"
+        ></span>
+  
+  
+  <h5>Trọng lượng:</h5>
+  <div class="parentIp__weight">
+    <input
+            id="input__weight"
+            class="pl-3"
+            type="number"
+            name="weight"
+            placeholder="trọng lượng"
+            disabled
+          />
+          <p> gram</p>
+          <span
+          class="sp-thongbao"
+          id="WeightError"
+        ></span>
+        
+  </div>
+          <p id="input__price" style="margin: 0">Giá trị: ${priceFormat} VND</p>
+          <button
+            id="button__submit"
+            type="button"
+            class="btn"
+            onclick="submitClick()"
+            
+          >
+            submit
+          </button>
+        </form>
+      </div>
     </div>
   </div>
-</div>
-  `;
-  document.getElementById("productRender").innerHTML = productContent;
+    `;
+
   document.getElementById("modaljs").innerHTML = modalContent;
+  document.getElementById("productRender").innerHTML = productContent;
 }
 
 function rendersidlebarProduct(data, number) {
   var sidlebarContent = "";
   for (let index = 0; index < 4; index++) {
     var productItem = data[index];
+    let priceFormat = numberFormat(data[index].price);
     sidlebarContent += `
   <div class="widget_tag_cloud">
   <div class="row media" href="#" >
@@ -177,7 +216,7 @@ function rendersidlebarProduct(data, number) {
 
     <div class="media-body col-sm-8">
       <h5 class="mt-0">${productItem.title}</h5>
-      <p>Giá :${productItem.price}VND</p>
+      <p>Giá :${priceFormat} VND</p>
       <button class="" href="#" onclick="pageProductAPI(${number},${
       +index + 1
     })">Xemchi tiet</button>
@@ -196,7 +235,7 @@ function submitClick() {
   var Area = document.getElementById("input__area").value;
   var Email = document.getElementById("input__email").value;
   var Phone = document.getElementById("input__phone").value;
-  var Count = document.getElementById("input__count").value;
+  var Weight = document.getElementById("input__weight").value;
   var isValid = true;
   isValid &=
     checkRequired(Name, "firstNameError") &&
@@ -214,13 +253,20 @@ function submitClick() {
     checkRequired(Phone, "phoneError") &&
     checkLenght(Phone, "phoneError", 1, 10);
   isValid &=
-    checkRequired(Count, "countError") &&
-    checkLenght(Count, "countError", 1, 2);
+    checkRequired(Weight, "WeightError") &&
+    checkLenght(Weight, "WeightError", 1, 3000);
   if (isValid) {
     alert("Đã gửi thông tin của bạn");
   }
 }
 
+//-------------close modal click----------------
+function closeModal() {
+  document.getElementById("input__reset").click();
+  document.getElementById("ip__count").innerText = 1;
+  document.getElementById("input__price").innerText = 0;
+  soLuong = 1;
+}
 function checkRequired(value, errorId) {
   if (value) {
     document.getElementById(errorId).innerHTML = "";
@@ -258,7 +304,16 @@ function checkString(value, errorId) {
   return false;
 }
 // end
-
+//Định dạng tiền Việt Nam
+function numberFormat(nStr) {
+  nStr += "";
+  x = nStr.split(".");
+  x1 = x[0];
+  x2 = x.length > 1 ? "." + x[1] : "";
+  var rgx = /(\d+)(\d{3})/;
+  while (rgx.test(x1)) x1 = x1.replace(rgx, "$1" + "," + "$2");
+  return x1 + x2;
+}
 // scorll ẩn hiện list product
 $(document).ready(function () {
   $(window).scroll(function (event) {
@@ -276,6 +331,48 @@ function pageProductAPI(key, id) {
   console.log(key);
   window.location.assign("blog.html?key=" + "product" + key + "/" + id);
 }
-function getWeight(weight) {
-  document.getElementById("input__weight").value = weight;
+function getWeight(gram, price) {
+  document.getElementById("input__weight").value = gram;
+
+  var priceFormat2 =
+    gram != 100 ? numberFormat(price * 2) : numberFormat(price);
+
+  document.getElementById("input__price").innerText =
+    "Giá trị : " + priceFormat2;
+}
+function changeCountPlus(price) {
+  // Tăng số Lượng
+  if (document.getElementById("input__weight").value != 0) {
+    document.getElementById("ip__count").innerText >= 20 ? 20 : soLuong++;
+    document.getElementById("ip__count").innerText = soLuong;
+    // conle.log(soLuong);
+    price =
+      document.getElementById("input__weight").value == 200 ? price * 2 : price;
+    price *= soLuong;
+    price = numberFormat(price);
+    var renderPrice = " Giá: " + price + " VND";
+    document.getElementById("input__price").innerHTML = renderPrice;
+  }
+}
+function changeCountMinus(price) {
+  //Giảm số Lượng
+  document.getElementById("ip__count").innerText == 1 ? 1 : soLuong--;
+
+  document.getElementById("ip__count").innerText = soLuong;
+  console.log(soLuong);
+  price *= soLuong;
+  console.log(price);
+  price = numberFormat(price);
+  var renderPrice = " Giá: " + price + " VND";
+  document.getElementById("input__price").innerHTML = renderPrice;
+}
+function submitProduct() {
+  console.log(productPost.name);
+  let name = document.getElementById("input__name").value;
+  let area = document.getElementById("input__area").value;
+  let email = document.getElementById("input__email").value;
+  let phone = document.getElementById("input__phone").value;
+  let count = document.getElementById("ip__count").value;
+  let weight = document.getElementById("input__weight").value;
+  let price = document.getElementById("input__price").value;
 }
